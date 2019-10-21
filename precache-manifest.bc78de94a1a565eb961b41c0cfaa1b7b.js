1a48b431f7c9f4457459063b5f7d6f924994e943
sieder villareal <sieder00@gmail.com>
2019-10-21 09:38:35 +0300

    Updates

---
+++ precache-manifest.bc78de94a1a565eb961b41c0cfaa1b7b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c774c71b5750057a59a1d52ded3881",
+    "url": "/svportfolio/index.html"
+  },
+  {
+    "revision": "f64d8b5510e16c1b42a1",
+    "url": "/svportfolio/static/css/main.dcd0b05b.chunk.css"
+  },
+  {
+    "revision": "6e84b64f63e1d7bec8ed",
+    "url": "/svportfolio/static/js/2.3d9a115d.chunk.js"
+  },
+  {
+    "revision": "f64d8b5510e16c1b42a1",
+    "url": "/svportfolio/static/js/main.cd03523b.chunk.js"
+  },
+  {
+    "revision": "5d9f5a58c569a35a9641",
+    "url": "/svportfolio/static/js/runtime-main.2d1ec737.js"
+  },
+  {
+    "revision": "4619c4562204e1d5771cc1091a56ae5d",
+    "url": "/svportfolio/static/media/loader.4619c456.svg"
+  },
+  {
+    "revision": "677632e9136731629b2633af7d78df3f",
+    "url": "/svportfolio/static/media/me.677632e9.svg"
+  },
+  {
+    "revision": "297392bb7fdcd86bcfe00ae0c19cb58c",
+    "url": "/svportfolio/static/media/moon.297392bb.svg"
+  },
+  {
+    "revision": "ecafbc86562d1bdf3404ded3d8f50be1",
+    "url": "/svportfolio/static/media/sun.ecafbc86.svg"
+  }
+]);
